c5bf58f814b55263be160fdec1ec448190563ce1
Vhmit <praxedes.9630@gmail.com>
2022-12-03 21:39:04 +0000

    X00P: Add prebuilt QuickPic Gallery for tree

--- device.mk
+++ device.mk
@@ -413,6 +413,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# Packages
+PRODUCT_PACKAGES += \
+    QPGallery
+
 # Dexpreopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Lawnchair \
